a4631e3b59771d5abd6a6471631ff928fc3d1728
William Song <47596959+william-song-shy@users.noreply.github.com>
2021-07-17 10:50:55 +0800

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,4 +1,9 @@
 import requests
+import datetime
+a=datetime.datetime.now()
 l=requests.get("http://bens.rotriw.com/ranklist?_contentOnly=1").json()
+#print ('./'+a.strftime('%Y-%m-%d %H-%M')+'.txt')
+fo=open('./'+a.strftime('%Y-%m-%d %H-%M')+'.txt','w',encoding="UTF-8")
 for i in l:
-    print (i[0],i[1],i[2])
+    fo.write (str(i[0])+' '+str(i[1])+' '+str(i[2])+'\n')
+fo.close()
