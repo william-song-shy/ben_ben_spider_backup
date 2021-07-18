559c59a816e5f42d90381a6b23b428810182ed82
William Song <47596959+william-song-shy@users.noreply.github.com>
2021-07-18 16:41:31 +0800

    Add files via upload

---
+++ backup_extend.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import datetime
+a=datetime.datetime.utcnow()+datetime.timedelta(hours=8)
+l=requests.get("https://bens.rotriw.com/api/backup/extend").json()
+#print ('./'+a.strftime('%Y-%m-%d %H-%M')+'.txt')
+if not os.path.exists("./backup_extend/"+a.strftime('%Y-%m-%d')+'/'):
+    os.mkdir("./backup_extend/"+a.strftime('%Y-%m-%d')+'/')
+fo=open('./backup_extend/'+a.strftime('%Y-%m-%d')+'/'+a.strftime('%Y-%m-%d %H-%M')+'.txt','w',encoding="UTF-8")
+for i in l['top_50']:
+    fo.write (str(i[0])+' '+str(i[1])+' '+str(i[2])+'\n')
+fo.write("\n\n\n\n\n\n====================\nTotal benbens:{}\nTotal users:{}".format(l['num_benben'],l['num_user']))
+fo.close()
